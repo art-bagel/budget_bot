052b5de993a5eb3c837fdeb03fae9b45a639a783
Alexander <mr.bagel@yandex.ru>
2024-02-20 18:27:51 +0300

    change docker-compose

--- database_scripts/Scripts/functions/create__recursive_transaction.sql
+++ database_scripts/Scripts/functions/create__recursive_transaction.sql
@@ -15,11 +15,12 @@ SET search_path TO 'prod';
 FOR cat IN (
 			select c.id as category_id, cg.group_id, c.name, cg."percent", c.is_group 
 			from categories c 
+				 join category_user cu on cu.category_id = c.id
 				 left join category_groups cg on cg.category_id = c.id
-			where c.is_income = false 
-				  and c.is_activ 
+			where c.is_income = false
+				  and c.is_activ
 				  and cg.group_id = _to_category_id
-				  and c.user_id = _user_id
+				  and cu.user_id = _user_id
 			)
 loop
 	if cat.is_group 
